ad33966c429b65f136e1bf9305feaf980bed646a
/Users/dmitry.pashinskiy/Desktop/ForFrameworks/Example/.git/config <dmitry.pashinskiy@appus.me>
2015-11-09 13:28:52 +0200

    Restruction of files

--- ADPPhotoPicker.podspec
+++ ADPPhotoPicker.podspec
@@ -29,6 +29,7 @@ Pod::Spec.new do |s|
   s.requires_arc = true
 
     s.source_files = 'Pod/Classes/**/*'
+
     s.resource_bundles = {
     'Resources' => ['Pod/Assets/**/*']
     }
--- Example/Pods/Target Support Files/Pods-ADPPhotoPicker_Tests/Pods-ADPPhotoPicker_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ADPPhotoPicker_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ADPPhotoPicker_TestsVersionString[];
-
